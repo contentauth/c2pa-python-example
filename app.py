9ca0406965f86887acb7f372a9ea4cbe1ba5832a
Tania Mathern <mathern@adobe.com>
2024-11-05 16:56:37 -0800

    More comments in example code

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 import io
 
 
-# Load env conf values
+# Load environment variable from .env file
 from dotenv import dotenv_values
 app_config = dotenv_values(".env")
 run_mode = app_config['RUN_MODE']
@@ -16,27 +16,30 @@ run_mode = app_config['RUN_MODE']
 app = Flask(__name__)
 
 
-# Loads env vars with a given prefix into APP config
+# Load env vars with a given prefix into APP config
 # By default, env vars with the `FLASK_`` prefix
 app.config.from_prefixed_env()
 
-# Load KMS key ID from local env
-# `create_kms_key` from the setup.py script created a key
-# with key spec ECC_NIST_P256
+# Load KMS key ID from local Flask app environment
+# `create_kms_key` from the setup.py script created a key with key spec ECC_NIST_P256
 kms_key_id = app.config["KMS_KEY_ID"]
 
-# Load the certificate chain from local env (chain.pem file)
+# Load the certificate chain from local Flask app environment (chain.pem file)
 cert_chain_path = app.config["CERT_CHAIN_PATH"]
 
 cert_chain = open(cert_chain_path, "rb").read()
 
 
 if run_mode == 'DEV':
+    # Development setup (eg. with LocalStack)
     endpoint_url = app_config['AWS_ENDPOINT']
     print(f'Running example in dev mode with endpoint: {endpoint_url}')
+
     region = app_config['REGION']
     aws_access_key_id = app_config['AWS_ACCESS_KEY_ID']
     aws_secret_access_key = app_config['AWS_SECRET_ACCESS_KEY']
+
+    # Use variables from .env file
     session = boto3.Session(region_name=region,
                             aws_access_key_id=aws_access_key_id,
                             aws_secret_access_key=aws_secret_access_key)
@@ -46,6 +49,7 @@ if run_mode == 'DEV':
                         aws_access_key_id=aws_access_key_id,
                         aws_secret_access_key=aws_secret_access_key)
 else:
+    # Example setup for use with AWS credentials setup (no LocalStack use)
     session = boto3.Session()
     kms = session.client('kms')
 
@@ -92,6 +96,7 @@ def resize():
                            cert_chain, "http://timestamp.digicert.com")
 
     result = io.BytesIO(b"")
+    # The result parameter is used to pass around the result of the builder.sign function
     builder.sign(signer, "image/jpeg", io.BytesIO(request_data), result)
 
     return result.getvalue()
@@ -99,4 +104,5 @@ def resize():
 
 def sign(data: bytes) -> bytes:
     hashed_data = sha256(data).digest()
+    # Uses KMS to sign
     return kms.sign(KeyId=kms_key_id, Message=hashed_data, MessageType="DIGEST", SigningAlgorithm="ECDSA_SHA_256")["Signature"]
